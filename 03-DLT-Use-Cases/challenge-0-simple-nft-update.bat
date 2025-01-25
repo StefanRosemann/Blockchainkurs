1dedf8fb1daca6b53181fb7a5fa031072781b8e6
Stefan Rosemann <info@stefan-rosemann.de>
2025-01-25 15:59:32 +0100

    copy

---
+++ 03-DLT-Use-Cases/challenge-0-simple-nft-update.bat
@@ -0,0 +1,5 @@
+copy bmw.jpg ..\..\..\challenge-0-simple-nft\packages\nextjs\public\
+copy page.tsx ..\..\..\challenge-0-simple-nft\packages\nextjs\app\
+copy nftsMetadata.ts ..\..\..\challenge-0-simple-nft\packages\nextjs\utils\simpleNFT\
+copy tailwind.config.js ..\..\..\challenge-0-simple-nft\packages\nextjs\
+
