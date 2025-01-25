c54732d2eaa10bf6f8fedf9730e1234081844599
Stefan Rosemann <info@stefan-rosemann.de>
2025-01-25 17:55:29 +0100

    Installation fertig

--- 03-DLT-Use-Cases/challenge-0-simple-nft-update.bat
+++ 03-DLT-Use-Cases/challenge-0-simple-nft-update.bat
@@ -1,5 +1,4 @@
-copy bmw.jpg ..\..\..\challenge-0-simple-nft\packages\nextjs\public\
-copy page.tsx ..\..\..\challenge-0-simple-nft\packages\nextjs\app\
-copy nftsMetadata.ts ..\..\..\challenge-0-simple-nft\packages\nextjs\utils\simpleNFT\
-copy tailwind.config.js ..\..\..\challenge-0-simple-nft\packages\nextjs\
-
+copy blockchainkurs\03-DLT-Use-Cases\challenge-0-simple-nft-update\bmw.jpg challenge-0-simple-nft\packages\nextjs\public\
+copy blockchainkurs\03-DLT-Use-Cases\challenge-0-simple-nft-update\page.tsx challenge-0-simple-nft\packages\nextjs\app\
+copy blockchainkurs\03-DLT-Use-Cases\challenge-0-simple-nft-update\nftsMetadata.ts challenge-0-simple-nft\packages\nextjs\utils\simpleNFT\
+copy blockchainkurs\03-DLT-Use-Cases\challenge-0-simple-nft-update\tailwind.config.js challenge-0-simple-nft\packages\nextjs\
