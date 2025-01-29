8c7f03a82dc0bc61d05fc2796c5b59c13e1d49ea
Stefan Rosemann <info@stefan-rosemann.de>
2025-01-29 12:46:12 +0100

    Verzeichnisstruktur

---
+++ 03-NFT/challenge-0-simple-nft-update.bat
@@ -0,0 +1,4 @@
+copy blockchainkurs\03-NFT\challenge-0-simple-nft-update\bmw.jpg challenge-0-simple-nft\packages\nextjs\public\
+copy blockchainkurs\03-NFT\challenge-0-simple-nft-update\page.tsx challenge-0-simple-nft\packages\nextjs\app\
+copy blockchainkurs\03-NFT\challenge-0-simple-nft-update\nftsMetadata.ts challenge-0-simple-nft\packages\nextjs\utils\simpleNFT\
+copy blockchainkurs\03-NFT\challenge-0-simple-nft-update\tailwind.config.js challenge-0-simple-nft\packages\nextjs\
